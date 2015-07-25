fa22913bb7220f16b305e5b4087eeabda7b5b8c2
d9magai <kumagai@sencorp.co.jp>
2015-07-26 00:57:13 +0900

    install wget

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,5 +1,9 @@
 # -*- coding: utf-8 -*-
 
+package "wget" do
+  action :install
+end
+
 execute "install google-chrome-stable" do
   command  "curl #{node["chrome"]["script_url"]} | sudo /bin/bash /dev/stdin -f"
   not_if "yum list installed | grep google-chrome-stable"
